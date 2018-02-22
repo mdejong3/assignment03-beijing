2214f09ed6281761fbba761d5b6a52c97d560da5
Melanie DeJong <melaniedejong42@gmail.com>
2018-02-21 21:04:07 -0500

    Added lorem ipsum

--- aziz.php
+++ aziz.php
@@ -22,12 +22,16 @@
 	<!--LAYOUT: it would be nice to keep these items in this order, since they're supposed to imitate a resume-->
 
 	<h3>Education</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis tortor imperdiet dolor molestie posuere. Morbi tincidunt lobortis dui a maximus. Curabitur vitae tempor lorem, sit amet ultricies dui. Morbi laoreet dui lacus, a convallis dolor hendrerit sed. Phasellus convallis posuere diam in pulvinar. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Fusce commodo nunc non consequat iaculis. Cras et commodo massa, non consequat sapien. Donec finibus pretium mi, vitae volutpat lectus porttitor non. Donec egestas sed risus at sodales. Ut ac hendrerit purus. Nam facilisis rutrum dolor sit amet pulvinar.</p>
 
 	<h3>Work Experience</h3>
+	<p>In pellentesque venenatis eros at suscipit. Cras consequat neque sit amet lacus sagittis varius nec et sem. Fusce pretium auctor placerat. Proin et pretium quam. Pellentesque at ligula lobortis, volutpat dolor non, fringilla ipsum. Donec rutrum blandit pulvinar. Morbi sed neque sagittis, mollis sapien vel, aliquam justo. Nam scelerisque tempor convallis. Curabitur fringilla est vitae ligula pharetra molestie. Aliquam blandit, dolor in malesuada tincidunt, dui odio tempus risus, vel lobortis felis ante eu felis. Aenean elementum sapien libero, id feugiat augue sollicitudin nec. Proin varius euismod nisi, ac lobortis justo iaculis ac. Sed feugiat vel mi vel auctor. Aenean a dignissim neque. Proin nec rhoncus massa, feugiat congue mi.</p>
 
 	<h3>Additional Experience</h3>
+	<p>Maecenas non dictum velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec aliquam nulla dolor, ac vehicula dolor lacinia quis. Curabitur at interdum odio, at vulputate lorem. In hac habitasse platea dictumst. Etiam et lectus fermentum, finibus nisi consectetur, ultricies tortor. Ut justo orci, ornare quis dolor a, porta elementum lorem. Vestibulum odio augue, volutpat id mauris eu, faucibus posuere metus. Curabitur vitae efficitur magna. Ut vel enim ac magna interdum convallis ut sed purus. Ut pharetra ex diam. Nunc magna nulla, pharetra sed velit at, gravida vehicula arcu.</p>
 	
 	<h3>Additional Skills</h3>
+	<p>In interdum nibh quis purus condimentum congue. Nullam mollis quis leo vitae vestibulum. Phasellus quis finibus ante. Nulla facilisi. In ac mauris sit amet ex maximus congue at id leo. Maecenas ultricies, ligula eget congue vulputate, felis nibh posuere metus, molestie malesuada nisl tortor mattis sem. Curabitur pharetra tortor sit amet feugiat dignissim. Mauris facilisis fringilla pharetra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam gravida eget purus et semper. Pellentesque eleifend volutpat tempor. Nullam facilisis libero eget iaculis facilisis.</p>
 	
 </div>
 
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ STYLING SUGGESTIONS:
 
 I picked these profiles on the basis that all of the guys are wearing suits in their pictures. So, I figured this would work well as a buisness-sytle website. 
 
-Typeface: something modern-looking would probably compliment the buisness-like aesthetic. 
+Typeface: something modern-looking would probably compliment the buisness-like aesthetic. Doing what we did in class with one custom font for the headers and another for the paragraph text would probably work well. 
 
 Color scheme: I was thinking that sticking to a fairly monochromatic scheme would help with the buisness feel.
 
@@ -34,15 +34,21 @@ Color scheme: I was thinking that sticking to a fairly monochromatic scheme woul
 <!--LAYOUT: I was thinking that these could be in three rows stacked on top of each other with the "read more" buttons off to the right so that they would be in the terminal area, though I'm not sure how that will look in practice-->
 
 	<h2>Oliver Ostriker</h2>
-	<p><!--small blurb about Oliver--></p>
+	<p><!--small blurb about Oliver-->
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis tortor imperdiet dolor molestie posuere. Morbi tincidunt lobortis dui a maximus. Curabitur vitae tempor lorem, sit amet ultricies dui. Morbi laoreet dui lacus, a convallis dolor hendrerit sed. Phasellus convallis posuere diam in pulvinar. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Fusce commodo nunc non consequat iaculis. Cras et commodo massa, non consequat sapien. Donec finibus pretium mi, vitae volutpat lectus porttitor non. Donec egestas sed risus at sodales. Ut ac hendrerit purus. Nam facilisis rutrum dolor sit amet pulvinar.
+	</p>
 	<a href="[Oliver's page]">Read more about Oliver</a>
 
 	<h2>Zubair Aziz</h2>
-	<p><!--small blurb about Zubair--></p>
+	<p><!--small blurb about Zubair-->
+		In pellentesque venenatis eros at suscipit. Cras consequat neque sit amet lacus sagittis varius nec et sem. Fusce pretium auctor placerat. Proin et pretium quam. Pellentesque at ligula lobortis, volutpat dolor non, fringilla ipsum. Donec rutrum blandit pulvinar. Morbi sed neque sagittis, mollis sapien vel, aliquam justo. Nam scelerisque tempor convallis. Curabitur fringilla est vitae ligula pharetra molestie. Aliquam blandit, dolor in malesuada tincidunt, dui odio tempus risus, vel lobortis felis ante eu felis. Aenean elementum sapien libero, id feugiat augue sollicitudin nec. Proin varius euismod nisi, ac lobortis justo iaculis ac. Sed feugiat vel mi vel auctor. Aenean a dignissim neque. Proin nec rhoncus massa, feugiat congue mi.
+	</p>
 	<a href="[Zubair's page]">Read more about Zubair</a>
 
 	<h2>Kennedy Agusi</h2>
-	<p><!--small blurb about Kennedy--></p>
+	<p><!--small blurb about Kennedy-->
+		Maecenas non dictum velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec aliquam nulla dolor, ac vehicula dolor lacinia quis. Curabitur at interdum odio, at vulputate lorem. In hac habitasse platea dictumst. Etiam et lectus fermentum, finibus nisi consectetur, ultricies tortor. Ut justo orci, ornare quis dolor a, porta elementum lorem. Vestibulum odio augue, volutpat id mauris eu, faucibus posuere metus. Curabitur vitae efficitur magna. Ut vel enim ac magna interdum convallis ut sed purus. Ut pharetra ex diam. Nunc magna nulla, pharetra sed velit at, gravida vehicula arcu.
+	</p>
 	<a href="[Kennedy's page]">Read more about Kennedy</a>
 
 </div>
