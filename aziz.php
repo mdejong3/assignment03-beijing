baddac1c01dd7b3d48b96011ecd28a91e8892569
jhirs10 <jhirs10@u.rochester.edu>
2018-02-24 21:28:59 -0500

    work in progress
    made a fundamental layout for the main 3 pages.  playing around with some colors and other things.  Everything is subject to change.

--- aziz.php
+++ aziz.php
@@ -13,12 +13,14 @@
 	<?php include('includes/nav.inc');?>
 	<h2>Zubair Aziz</h2>
 
-	<figure>
-		<img src="images/zubair.jpg" alt="Zubair Aziz"> <!--I'll try to make these better quality-->
-		<figcaption>Zubair Aziz</figcaption>
-	</figure>
+
 
 	<!--LAYOUT: it would be nice to keep these items in this order, since they're supposed to imitate a resume-->
+<article>
+<div class="box">
+	<div class="row">
+
+	<div class="col">
 
 	<h3>Education</h3>
 		<ul>
@@ -26,23 +28,50 @@
 			<li>Majors: Economics, Computer Science</li>
 			<li>Minors: None</li>
 		</ul>
+	</div>
 
+	<div class="col">
+	<div class="list2">
 	<h3>Skills</h3>
 		<ul>
 			<li>Experience working in a dev team</li>
 			<li>Optimizing webpages for better load speeds and search rankings</li>
 			<li>Painting, specializing in landscapes</li>
 		</ul>
+	</div>
+</div>
 
+	<div class="col">
+	<figure>
+		<img src="images/zubair.jpg" alt="Zubair Aziz"> <!--I'll try to make these better quality-->
+		<figcaption>Zubair Aziz</figcaption>
+	</figure>
+</div>
+</div>
+</div>
+</article>
+
+	<div class="top-buffer">
+	<div class="row">
+	<div class="col-md-8">
+<div class="box">
 	<h3>Work Experience</h3>
 		<h4>University of Rochester IT Consultant</h4>
 			<p>As an IT Consultant at the U of R, I assist with network registration and troubleshoot network issues, troubleshoot NetID and login issues, provide support through various methods,assist with various types of malware on students' personal computers.</p>
 		<h4>Media Prima Digital Developer Intern</h4>
 			<p>At Media Prima Digital, I worked with the team to develop modern websites and apps for various different uses, and was trained in both front end and back end systems for web development.</p>
+</div>
+	</div>
 
+
+	<div class="col-md-4">
+<div class="box">
 	<h3>Additional Experience</h3>
 	<p>I'm involved in some extra-curricular activities such as Alpha Kappa Psi, the professional business fraternity; and Grass Roots Soccer, a health organization that uses the power of soccer to educate, inspire, and mobilize young people to lead healthier lives. </p>
-	
+	</div>
+	</div>
+	</div>
+</div>	
 </div>
 
 </body>
--- ostriker.php
+++ ostriker.php
@@ -14,14 +14,16 @@
 
 	<h2>Oliver Ostriker</h2>
 
-	<figure>
-		<img src="images/oliver.png" alt="Oliver Ostriker"> <!--I'll try to make these better quality-->
-		<figcaption>Oliver Ostriker</figcaption>
-	</figure>
+
 
 	<!--LAYOUT: it would be nice to keep these items in this order, since they're supposed to imitate a resume-->
 
-	<div class="col-md-4">
+<article>
+<div class="box">
+	<div class="row">
+
+	<div class="col">
+
 	<h3>Education</h3>
 		<ul>
 			<li>School: University of Rochester, anticipated graduation 2019</li>
@@ -30,7 +32,9 @@
 		</ul>
 	</div>
 
-	<div class="col-md-4">
+
+	<div class="col">
+	<div class="list2">
 	<h3>Skills</h3>
 		<ul>
 			<li>Teaching experience</li>
@@ -38,15 +42,43 @@
 			<li>Skillful time-management</li>
 		</ul>
 	</div>
+</div>
 
+
+	<div class="col">
+	<figure>
+		<img src="images/oliver.png" alt="Oliver Ostriker"> <!--I'll try to make these better quality-->
+		<figcaption>Oliver Ostriker</figcaption>
+	</figure>
+	</div>
+</div>
+
+	</div>
+</article>
+
+	<div class="top-buffer">
+	<div class="row">
+	<div class="col-md-8">
+<div class="box">
 	<h3>Work Experience</h3>
 		<h4>Skyview Ventures LCC Intern</h4>
 			<p>As an intern at Skyview Ventures LLC, a renewable energy finance, development and asset management company, I learned about the solar market through research and analysis into government policies that affect the supply and demand of Solar Renewable Energy Credits (SRECs).</p>
 		<h4>Camp Gan Israel Camp Counselor</h4>
 			<p>At Camp Gan Israel, me and one other counselor lead a group of about 15 five and six year olds through various activities, including arts and crafts and sports. </p>
+</div>
+	</div>
 
+
+	<div class="col-md-4">
+<div class="box">
 	<h3>Additional Experience</h3>
 	<p>On campus, I have experience working as a Program Assistant for the University of Rochester’s Center for Emerging and Innovative Sciences (CEIS), a program that promotes economic development in the upstate New York region. Here, I had the opportunity to conduct research into the optics and photonics industries of upstate New York, and to help organize and set-up both the 2017 CEIS Technology Showcase and the 2017 Light and Sound Interactive, conferences that attracted hundreds of members of the optics and photonics industries including university researchers and executives from companies with areas of common interest.</p>
+	</div>
+	</div>
+	</div>
+</div>
+
+
 
 </div>
 
