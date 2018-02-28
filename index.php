846e8d2d1821cedb09c272b257461b742c565d51
benschmitz <benschmitz1997@gmail.com>
2018-02-27 20:45:09 -0500

    Added caotion to slideshow and updated colophon

--- index.php
+++ index.php
@@ -27,9 +27,9 @@ Color scheme: I was thinking that sticking to a fairly monochromatic scheme woul
 
 	<div class="slideshow">
 		<ul>
-			<li><a href="ostriker.php"><img class="full" src="images/oliver.png" alt="John Mcafee"></a></li>
-			<li><a href="aziz.php"><img class="full" src="images/zubair.jpg" alt="Grace Hopper"></a></li>
-			<li><a href="agusi.php"><img class="full" src="images/kennedy.jpg" alt="Dennis Ritchie"></a></li>
+			<li><a href="ostriker.php"><img class="full" src="images/oliver.png" alt="Oliver Ostriker"></a><figcaption>Oliver Ostriker</figcaption></li>
+			<li><a href="aziz.php"><img class="full" src="images/zubair.jpg" alt="Zubair Aziz"></a><figcaption>Zubair Aziz</figcaption></li>
+			<li><a href="agusi.php"><img class="full" src="images/kennedy.jpg" alt="Kennedy Agusi"></a><figcaption>Kennedy Agusi</figcaption></li>
 		</ul>
 	</div>
 
--- readme.md
+++ readme.md
@@ -13,4 +13,4 @@ Melanie DeJong - selected student profiles to use in webstie, pulled relevant in
 Jonathan Hirsch - Created all css styles, and implemented the CSS library 'Bootstrap' to create the structure for all 4 pages.  Also the fonts Bellefair (for the titles) and Roboto (for the body) were installed and according to google fonts they pair well together.
 
 ## Coder
-Ben Schmitz
+Ben Schmitz - Implemented JS slideshow on front page and setup PHP for current tab display on nav element. Also validated HTML and CSS files and assured that file system was clean.
